e0c877697a18d1c8e37452528775cfaf4aa5c9aa
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-10-09 01:29:15 +0300

    set keymap for lazy

--- init.lua
+++ init.lua
@@ -226,6 +226,7 @@ vim.opt.rtp:prepend(lazypath)
 --  To update plugins you can run
 --    :Lazy update
 --
+vim.keymap.set('n', '<leader>l', '<cmd>Lazy<CR>', { desc = 'Lazy' })
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
