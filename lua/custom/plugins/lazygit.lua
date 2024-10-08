f4b758609def52da735eea7942a046c3e4b3337b
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-10-09 00:35:43 +0300

    add lazygit

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+}
