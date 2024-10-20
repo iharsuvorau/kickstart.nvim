17fa7f4e7326d4a30a3f3c58a1d468c174ba39d2
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-10-20 10:36:38 +0300

    set norg width at 80

--- init.lua
+++ init.lua
@@ -150,6 +150,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('filetype-specific-settings', { clear = true }),
+  pattern = { 'norg' },
+  callback = function()
+    vim.opt_local.textwidth = 80
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
