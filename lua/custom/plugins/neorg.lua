7514f732651ad26753af553d1499bad5264c0d4a
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-10-20 10:37:31 +0300

    configure norg

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -2,5 +2,17 @@ return {
   'nvim-neorg/neorg',
   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
   version = '*', -- Pin Neorg to the latest stable release
+  opts = {
+    load = {
+      ['core.defaults'] = {},
+      ['core.concealer'] = {},
+      ['core.export'] = {},
+      ['core.export.markdown'] = {
+        config = {
+          extensions = 'all',
+        },
+      },
+    },
+  },
   config = true,
 }
