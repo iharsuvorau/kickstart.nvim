ee4862867861fe285c653f8963ac24a01657ca46
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-11-19 10:02:24 +0200

    add markdown and copilot chat keys

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -12,5 +12,32 @@ return {
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
+    keys = {
+      {
+        '<leader>ctt',
+        function()
+          require('CopilotChat').open()
+        end,
+        desc = 'CopilotChat - Open',
+      },
+      {
+        '<leader>ctq',
+        function()
+          local input = vim.fn.input 'Quick Chat: '
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          end
+        end,
+        desc = 'CopilotChat - Quick chat',
+      },
+      {
+        '<leader>ctp',
+        function()
+          local actions = require 'CopilotChat.actions'
+          require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
+        end,
+        desc = 'CopilotChat - Prompt actions',
+      },
+    },
   },
 }
