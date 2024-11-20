1c412fddb4f540d5e137439c1ca2564610d2ba4f
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-11-20 23:55:36 +0200

    refactor init.lua

---
+++ plugin/autocommands.lua
@@ -0,0 +1,49 @@
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  group = vim.api.nvim_create_augroup('filetype-specific-settings', { clear = true }),
+  pattern = { 'norg' },
+  callback = function()
+    vim.opt_local.textwidth = 80
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact' },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
+-- Run organize go imports on file save
+local gofile_au_group = vim.api.nvim_create_augroup('goimports-on-write', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = gofile_au_group,
+  pattern = '*.go',
+  callback = function()
+    vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
+    --vim.lsp.buf.code_action { context = { only = { 'source.fixAll' } }, apply = true }
+  end,
+  desc = 'Organize go imports on save',
+})
+
+-- Set file type for Jinja2 files
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+  group = vim.api.nvim_create_augroup('jinja-on-open', { clear = true }),
+  pattern = { '*.jinja', '*.jinja2', '*.j2' },
+  command = 'set filetype=jinja',
+})
