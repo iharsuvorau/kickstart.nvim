c70cd317ceb79940b9b723860d1b0f33029c2eee
Ihar Suvorau <ihar.suvorau@apromore.com>
2024-12-05 16:18:06 +0200

    configure oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,5 +2,15 @@ return {
   'stevearc/oil.nvim',
   opts = {
     default_file_explorer = true,
+    delete_to_trash = true,
+    keymaps = {
+      ['gd'] = function()
+        require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+      end,
+      ['<C-r>'] = 'refresh',
+    },
+  },
+  keys = {
+    { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
   },
 }
