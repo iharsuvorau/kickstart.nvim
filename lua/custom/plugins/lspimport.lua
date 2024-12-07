4dcc4aca67621b8d07058e1185179f7fa29b267d
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-12-07 13:26:56 +0200

    remap lspimport error fix

--- lua/custom/plugins/lspimport.lua
+++ lua/custom/plugins/lspimport.lua
@@ -1,6 +1,6 @@
 return {
   'stevanmilic/nvim-lspimport',
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>pi', ':lua require("lspimport").import()<CR>', { noremap = true })
+    vim.api.nvim_set_keymap('n', '<leader>i', ':lua require("lspimport").import()<CR>', { desc = 'Resolve import errors', noremap = true })
   end,
 }
