6ae638d2af27efc1761264f8e88f131dbc9d75dd
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-12-07 13:26:39 +0200

    add ugaterm

---
+++ lua/custom/plugins/ugaterm.lua
@@ -0,0 +1,7 @@
+return {
+  'uga-rosa/ugaterm.nvim',
+  keys = {
+    { '<leader>to', '<cmd>UgatermOpen<cr>', desc = 'Open terminal' },
+    { '<leader>th', '<cmd>UgatermHide<cr>', desc = 'Hide terminal' },
+  },
+}
