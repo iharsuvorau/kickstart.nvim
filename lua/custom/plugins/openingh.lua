f8cd2d8cc6cd73cc4f3d389827e232d915edee32
Ihar Suvorau <ihar.suvorau@gmail.com>
2025-01-05 15:38:44 +0200

    remove openingh

--- lua/custom/plugins/openingh.lua
+++
@@ -1,7 +0,0 @@
-return {
-  -- 'Almo7aya/openingh.nvim',
-  -- keys = {
-  --   { '<leader>gr', '<cmd>OpenInGHRepo<cr>', desc = 'Open GitHub repository' },
-  --   { '<leader>gf', '<cmd>OpenInGHFile<cr>', desc = 'Open GitHub file' },
-  -- },
-}
