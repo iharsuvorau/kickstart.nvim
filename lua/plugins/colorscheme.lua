d9009ae1d81d7bc5ec9700f223049b30dfbe4c12
Ihar Suvorau <ihar.suvorau@gmail.com>
2025-05-21 22:17:33 +0200

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,20 @@
 return {
   -- {
-  --   'rafi/awesome-vim-colorschemes',
-  --   lazy = false,
-  --   priority = 1000,
+  --   'projekt0n/github-nvim-theme',
+  --   name = 'github-theme',
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
   --   config = function()
-  --     vim.cmd.colorscheme 'tender'
-  --   end,
-  -- },
-  -- {
-  --   'mikesmithgh/gruvsquirrel.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   opts = {},
-  --   config = function()
-  --     vim.cmd.colorscheme 'gruvsquirrel'
+  --     vim.cmd 'colorscheme github_dark_dimmed'
   --   end,
   -- },
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    'mikesmithgh/gruvsquirrel.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
     config = function()
-      vim.cmd 'colorscheme github_dark_dimmed'
+      vim.cmd.colorscheme 'gruvsquirrel'
     end,
   },
 }
