a68ba5b1b2f8aca45e2b4d13b02843f6cb238bcf
Ihar Suvorau <ihar.suvorau@apromore.com>
2025-06-02 16:12:55 +0300

    fix github-opener

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,7 +2,6 @@ return {
   {
     dir = vim.fn.stdpath 'config' .. '/lua/local/github-opener',
     config = function()
-      require 'github-opener'
       vim.keymap.set('n', '<leader>gh', ':OpenGitHub<cr>', { noremap = true, silent = true, desc = 'Open GitHub' })
       vim.keymap.set('n', '<leader>gf', ':OpenGitHubFile<cr>', { noremap = true, silent = true, desc = 'Open file in GitHub' })
     end,
