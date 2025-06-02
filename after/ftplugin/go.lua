b3e1bf4237e6af6d10f1e04cac8f41ec414b479e
Ihar Suvorau <ihar.suvorau@gmail.com>
2025-06-02 22:30:01 +0300

    fix go after ft plugin

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,3 +1,3 @@
-vim.opt_local.set.expandtab = false
-vim.opt_local.set.tabstop = 4
-vim.opt_local.set.shiftwidth = 4
+vim.opt_local.expandtab = false
+vim.opt_local.tabstop = 4
+vim.opt_local.shiftwidth = 4
